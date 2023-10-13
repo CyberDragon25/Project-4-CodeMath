8d2ce9c9a1d75eacdb1162a37accef0550fbc0aa
Shubham <bhattacharya.shubham123@gmail.com>
2023-10-12 23:00:09 -0500

    Created files in Kotilin

---
+++ app/src/main/java/edu/iastate/shubham8/project4_codemath/MainActivity.kt
@@ -0,0 +1,11 @@
+package edu.iastate.shubham8.project4_codemath
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
